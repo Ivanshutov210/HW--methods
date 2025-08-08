25d701e7d1b8c63ca5166f1851c9a77815f7ae78
Иван <ivan.shutov95@mail.ru>
2025-08-08 09:10:19 +0300

    Hw 2

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,27 @@
 public class Main {
     public static void main(String[] args) {
     }
-        public static void calculateLeapYear(int year) {
-            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-                System.out.println(year + " год — високосный год");
-            } else {
-                System.out.println(year + " год — невисокосный год");
-            }
-        }
-        }
 
+    // Задача 1
+    public static void calculateLeapYear(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " год — високосный год");
+        } else {
+            System.out.println(year + " год — невисокосный год");
+        }
+    }
+
+    // Задача 2
+    public static void installingOsAndYear(int clientOs, int clientDeviceYear) {
+        if (clientOs == 0 && clientDeviceYear < 2025) {
+            System.out.println("Установите облегченную версию приложения для системы iOS по ссылке");
+        } else if (clientOs == 0) {
+            System.out.println("Установите приложения для системы iOS по ссылке");
+        }
+        if (clientOs == 1 && clientDeviceYear < 2025) {
+            System.out.println("Установите облегченную версию приложения для системы Android по ссылке");
+        } else if (clientOs == 1) {
+            System.out.println("Установите приложения для системы Android по ссылке");
+        }
+    }
+}
